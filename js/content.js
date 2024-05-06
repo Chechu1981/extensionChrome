7ae96fcff6f8a62f0b51b8920b04ea06be84da01
Chechu1981 <jesus@jesusma.es>
2024-05-06 18:02:54 +0200

    new option on chargue

--- js/content.js
+++ js/content.js
@@ -1,11 +1,12 @@
 'use strict';
 
-let pagina = false
-
-window.location.href.includes('google') ?
-  pagina = true : pagina = false
-
-if (pagina){
-  const parrafos = document.querySelectorAll('div')
-  console.log(parrafos)
+if (window.location.href.includes('power-supply')){
+  const parrafos = document.getElementsByClassName('psa-order-content-line')
+  let correo = `${parrafos[23].childNodes[1].innerText.replaceAll('','').replaceAll('\n','')};${parrafos[27].childNodes[1].innerText.replaceAll('','').replaceAll('\n','')}`
+  let referencia = parrafos[4].innerHTML
+  let cantidad = parrafos[4].innerHTML
+  let cuenta = parrafos[4].innerHTML
+  let cliente = parrafos[4].innerHTML
+  let fecha = parrafos[4].innerHTML
+  alert(correo)
 }
--- js/index.js
+++ js/index.js
@@ -7,3 +7,12 @@ chrome.action.onClicked.addListener((tab) => {
     files: ['./js/content.js']
   })
 })
+
+chrome.action.onLoaded.addListener((tab) => {
+  alert(tab)
+})
+/*
+if (chrome.action.location.href.includes('power-supply')){
+  const parrafos = document.getElementsByClassName('psa-order-content-line')
+  parrafos[5].innerHTML = '<button>Hola mundo</button>'
+}*/
